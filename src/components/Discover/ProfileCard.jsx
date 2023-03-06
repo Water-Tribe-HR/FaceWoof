405f87474188370c02bb697a5245e724a2352551
Gabe Bennett-Brandt <gbennettbrandt@gmail.com>
2023-03-06 13:58:27 -0600

    Revert "Queries update - hopefully github issue fixed"

--- src/components/Discover/ProfileCard.jsx
+++ src/components/Discover/ProfileCard.jsx
@@ -3,8 +3,11 @@ import React, { useEffect, useState } from 'react';
 import './profileCard.css';
 
 export default function ProfileCard() {
+  const imgStyle = {
+    scale: 0.5,
+  };
+
   const user = {
-    profile_photo: 'https://i.ibb.co/VCX4GWs/KOA-Nassau-2697x1517.jpg',
     photos: [
       'https://i.ibb.co/VCX4GWs/KOA-Nassau-2697x1517.jpg',
       'https://i.ibb.co/k4rMVRK/dog-puppy-on-garden-royalty-free-image-1586966191.jpg',
@@ -19,9 +22,8 @@ export default function ProfileCard() {
       'long walks',
     ],
     bio: 'If a dog chews shoes whose shoes does he choose?',
-  };
 
-  const distance = 5;
+  };
 
   return (
     <div>
@@ -30,63 +32,50 @@ export default function ProfileCard() {
           <div className="card-header-title">
             <div className="avatar">
               <div className="w-24 rounded-contain profile-image">
-                <img src={user.profile_photo} />
+                <img src="https://i.ibb.co/VCX4GWs/KOA-Nassau-2697x1517.jpg" />
               </div>
             </div>
 
             <div className="profile-details">
               <div className="card-body-top">
                 <h2 className="card-title-text">
-                  {user.username}
+                  Buster
                 </h2>
-                <p className="profile-card-details">
-                  {user.age}
-                  {' '}
-                  •
-                  {' '}
-                  {user.breed}
-                  {' '}
-                  •
-                  {' '}
-                  {distance}
-                  {' '}
-                  miles
-                </p>
-                {
-                  user.vaccinated
-                    ? <div className="badge badge-secondary">&#10004; Vaccinated</div>
-                    : null
-                }
+                <p className="profile-card-details">6 months • Golden Retriever • 5 miles</p>
+                <div className="badge badge-secondary">&#10004; Vaccinated</div>
               </div>
             </div>
 
           </div>
           <div className="card-actions justify-start">
-            {
-              user.interests.map((interest) => (
-                <div className="badge badge-outline">{interest}</div>
-              ))
-            }
+            <div className="badge badge-outline">Squirrels</div>
+            <div className="badge badge-outline">Bones</div>
           </div>
         </div>
         <div className="carousel-container">
           <figure>
+            {/* <img src="https://i.ibb.co/VCX4GWs/KOA-Nassau-2697x1517.jpg" alt="Shoes" /> */}
             <div className="carousel w-full">
-              {
-                user.photos.map((url, index) => (
-                  <div id={`item${index}`} className="carousel-item w-full">
-                    <img className="w-full" src={url} alt="Doggy" />
-                  </div>
-                ))
-              }
+              <div id="item1" className="carousel-item w-full">
+                <img className="w-full" src="https://i.ibb.co/VCX4GWs/KOA-Nassau-2697x1517.jpg" alt="Burger" />
+              </div>
+              <div id="item2" className="carousel-item w-full">
+                <img className="w-full" src="https://i.ibb.co/k4rMVRK/dog-puppy-on-garden-royalty-free-image-1586966191.jpg" alt="Burger" />
+              </div>
+              <div id="item3" className="carousel-item w-full">
+                <img className="w-full" src="https://i.ibb.co/VCX4GWs/KOA-Nassau-2697x1517.jpg" alt="Burger" />
+              </div>
+              <div id="item4" className="carousel-item w-full">
+                <img className="w-full" src="https://i.ibb.co/VCX4GWs/KOA-Nassau-2697x1517.jpg" alt="Burger" />
+              </div>
             </div>
+
           </figure>
           <div className="flex justify-center w-full py-2 gap-2 carousel-buttons">
-            {
-                user.photos.map((url, index) => (
-                  <a href={`#item${index}`} className="btn btn-xs">{index}</a>
-                ))
-            }
+            <a href="#item1" className="btn btn-xs">1</a>
+            <a href="#item2" className="btn btn-xs">2</a>
+            <a href="#item3" className="btn btn-xs">3</a>
+            <a href="#item4" className="btn btn-xs">4</a>
           </div>
         </div>
         <div className="card-body card-bottom">
@@ -94,7 +83,7 @@ export default function ProfileCard() {
             About Buster:
             <div className="badge badge-secondary">NEW</div>
           </h2> */}
-          <p>{user.bio}</p>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
         </div>
       </div>
     </div>
--- src/views/Discover.jsx
+++ src/views/Discover.jsx
@@ -1,19 +1,13 @@
-/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 import ProfileCard from '../components/Discover/ProfileCard';
-import './discover.css';
 
 function Discover() {
   return (
     <div>
       <h1>Discover</h1>
-      <div className="discover-cardview-parent">
-        <button type="button" className="btn btn-active btn-accent">Pass</button>
-        <ProfileCard />
-        <button type="button" className="btn btn-active btn-primary">Digg &apos;em</button>
-      </div>
+      <ProfileCard />
     </div>
   );
 }
